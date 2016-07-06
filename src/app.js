0f17a82fb9e70d174aefba358ef531c0591b5581
alvarollmenezes <alvarollmenezes@gmail.com>
2016-07-06 17:38:40 -0300

    Allow Authorization header CORS rule

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use( compress() );
 // Enable CORS http://enable-cors.org/server_expressjs.html
 app.use( ( req, res, next ) => {
     res.header( 'Access-Control-Allow-Origin', '*' );
-    res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
+    res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization' );
     next();
 } );
 
