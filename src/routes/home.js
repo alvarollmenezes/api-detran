3df0f7656a0911357907767d4aa35c9a903cf495
alvarollmenezes <alvarollmenezes@gmail.com>
2016-07-06 17:48:23 -0300

    Fix

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,7 @@ module.exports = app => {
 
     const homeController = require( '../controllers/homeController' )();
 
-    app.get( '/driverData', homeController.getDriverLicenseData );
+    app.get( '/driverData', homeController.getDriverData );
 
     app.get( '/tickets', homeController.getTickets );
 
